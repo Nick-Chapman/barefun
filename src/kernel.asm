5a1da94bafb7d962371848cadf542d9b1868276d
Nick Chapman <nic@amadido.co.uk>
2025-02-12 23:09:12 +0000

    explicit continuation management

--- src/kernel.asm
+++ src/kernel.asm
@@ -7,137 +7,65 @@
     jmp start
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; current continuation (register bp)
+
+%macro kret 0
+    jmp bp
+%endmacro
+
+%macro kcall 1
+    push bp
+    mov bp, %%after
+    jmp %1
+%%after:
+    pop bp
+%endmacro
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; start
 
-return_stack_base equ 0 ; top of memory
-param_stack_base equ 0xfc00 ; allowing 1k for return stack (512 cells)
+top_of_memory equ 0
 
 start:
-    mov sp, return_stack_base
-    mov bp, param_stack_base
-    call clear_screen
+    mov sp, top_of_memory
+    kcall clear_screen
 .loop:
-    call _key
-    ;;call _dup
-    ;;call _emit
-    call _emit
+    kcall read_char
+    push ax
+    kcall emit
+    pop ax
+    kcall emit
     jmp .loop
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; print macro
-
-%macro print 1
-    push di
-    jmp %%after
-%%message: db %1, 0
-%%after:
-    mov di, %%message
-    call print_string
-    pop di
-%endmacro
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Parameter stack (register: bp)
-
-%macro pspush 1
-    sub bp, 2
-    mov [bp], %1
-%endmacro
-
-check_ps_underflow:
-    cmp bp, param_stack_base
-    jb .ok
-    sub bp, 2
-    mov word [bp], 0
-    print "stack underflow."
-    call output_newline
-    call crash
-.ok:
-    ret
-
-%macro pspop 1
-    call check_ps_underflow
-    mov %1, [bp]
-    add bp, 2
-%endmacro
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; underscore ops; use parameter stack
-
-_key:
-    call read_char
-    mov ah, 0
-    pspush ax
-    ret
-
-_dup:
-    mov ax, [bp]
-    mov [bp-2], ax
-    sub bp, 2
-    ret
-
-_emit:
-    pspop ax
+emit:
     cmp ax, 13
     jz output_newline
     cmp ax, 10
     jz output_newline
     jmp output_char
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; non-underscore ops -- use specific regs
-
-print_string: ; in: DI=string; print null-terminated string.
-    push ax
-    push di
-.loop:
-    mov al, [di]
-    cmp al, 0 ; null?
-    je .done
-    call output_char
-    inc di
-    jmp .loop
-.done:
-    pop di
-    pop ax
-    ret
-
-crash:
-    print "We have crashed. (any key will quit)"
-    call output_newline
-    call read_char ; wait for a key to be pressed, then quit system
-    jmp quit
-
 output_newline:
     mov al,  10
-    call output_char
+    kcall output_char
     mov al,  13
-    call output_char
-    ret
+    jmp output_char
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; primitive BIOS ops
 
-quit:
-    mov ax, 0x5307
-    mov bx, 0x0001
-    mov cx, 0x0003
-    int 0x15
-.loop:
-    jmp .loop
-
 clear_screen:
     mov ax, 0x0003 ; AH=0 AL=3 video mode 80x25
     int 0x10
-    ret
+    kret
 
 output_char: ; in: AL=char
     mov ah, 0x0e ; Function: Teletype output
     mov bh, 0
     int 0x10
-    ret
+    kret
 
-read_char: ; out: AL=char
+read_char: ; out: AX=char
     mov ah, 0
     int 0x16
-    ret
+    mov ah, 0
+    kret
