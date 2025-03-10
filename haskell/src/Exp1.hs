1a817acc881b0a6009e377b88c088e34a144b6a0
Nick Chapman <nic@amadido.co.uk>
2025-03-10 14:34:23 +0000

    first cut pretty printer

--- haskell/src/Exp1.hs
+++ haskell/src/Exp1.hs
@@ -1,4 +1,7 @@
-module Exp1 where
+module Exp1 (Exp(..),Arm(..),Literal(..),Id(..),Cid(..)) where
+
+import Data.List (intercalate)
+import Text.Printf (printf)
 
 data Exp
   = Var Id
@@ -9,21 +12,92 @@ data Exp
   | Let Id Exp Exp
   | Prim Builtin [Exp]
   | Case Exp [Arm]
-  deriving (Show)
 
 data Arm = Arm Cid [Id] Exp
-  deriving (Show)
 
 data Literal = LitC Char
-  deriving (Show)
 
-data Builtin = PutChar | GetChar | EqChar
-  deriving (Show)
+data Builtin -- = PutChar | GetChar | EqChar -- TODO
 
 newtype Id = Id String
-  deriving (Show)
-
 newtype Cid = Cid String
-  deriving (Show)
 
--- TODO: pretty printer
+instance Show Exp where show = intercalate "\n" . pretty
+instance Show Cid where show (Cid s) = s
+instance Show Id where show (Id s) = s
+instance Show Literal where show = \case LitC c -> show c
+
+{- TODO
+example should use constructor syntax for Nil/Cons
+"rec", parse and pp
+unit, parse as constructor, and special case PP
+-}
+
+pretty :: Exp -> Lines
+pretty = \case
+
+  Let x rhs body ->
+    indented ("let " ++ show x ++ " =") (onTail (++ " in") (pretty rhs))
+    ++ pretty body
+
+  Lam x body ->
+    bracket $
+    indented ("fun " ++ show x ++ " ->") (pretty body)
+
+  App e1 e2 ->
+    bracket $
+    jux (pretty e1) (pretty e2)
+
+  Var x -> [show x]
+
+  Con c [] ->
+    [show c]
+
+  Con c es ->
+    undefined $ onHead (show c ++) (foldl juxComma [] (map pretty es)) -- TODO
+
+  Lit x ->
+    [show x]
+
+  Case scrut arms ->
+    (onHead ("match "++) . onTail (++ " with")) (pretty scrut)
+    ++ concat (map prettyArm arms)
+
+  Prim b es ->
+    undefined b es -- TODO
+
+
+prettyArm :: Arm -> Lines
+prettyArm = \case
+  Arm c xs rhs -> do
+    indented ("| " ++ prettyPat c xs ++ " ->") (pretty rhs)
+
+prettyPat :: Cid -> [Id] -> String
+prettyPat c = \case
+  [] -> show c
+  xs -> printf "%s(%s)" (show c) (intercalate "," (map show xs))
+
+juxComma :: Lines -> Lines -> Lines
+juxComma a b = jux (onTail (++",") a) b
+
+type Lines = [String]
+
+bracket :: Lines -> Lines
+bracket = onHead ("(" ++) . onTail (++ ")")
+
+onHead,onTail :: (String -> String) -> Lines -> Lines
+onHead _ [] = error "onHead"
+onHead f (x:xs) = f x : xs
+onTail f = reverse . onHead f . reverse
+
+jux :: Lines -> Lines -> Lines
+jux [x] [y] = [ x ++ " " ++ y ]
+jux xs ys = xs ++ ys
+
+indented :: String -> Lines -> Lines
+indented hang = \case
+  [] -> error "indented"
+  [oneLine] -> [hang ++ " " ++ oneLine]
+  lines -> [hang] ++ ["  " ++ line | line <- lines]
+
+
