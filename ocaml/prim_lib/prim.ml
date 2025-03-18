b41b92f5b63c7a321ca049ae8ec6778d3258c8ea
Nick Chapman <nic@amadido.co.uk>
2025-03-18 13:43:09 +0000

    ocaml: explicit signature for supported primitives

--- ocaml/prim_lib/prim.ml
+++ ocaml/prim_lib/prim.ml
@@ -1,42 +1,52 @@
+module X : sig
 
-(* TODO: define the Prim signature explicitly *)
+  val ( * ) : int -> int -> int
+  val (%) : int -> int -> int
+  val (+) : int -> int -> int
+  val (-) : int -> int -> int
+  val (/) : int -> int -> int
+  val chr : int -> char
+  val eq_char : char -> char -> bool
+  val eq_int : int -> int -> bool
+  val explode : string -> char list
+  val get_char : unit -> char
+  val less_int : int -> int -> bool
+  val ord : char -> int
+  val put_char : char -> unit
+  val upto : int -> int -> int list
 
-let eq_char : char -> char -> bool = fun c1 c2 ->
-  c1 = c2
+end = struct
 
-let get_char : unit -> char = fun () ->
-  let bytes = Bytes.create 1 in
-  let n = Unix.read Unix.stdin bytes 0 1 in
-  if n < 1 then (Printf.printf "[EOF]\n"; exit 0) else
-  let c : char = Bytes.get bytes 0 in
-  c
+  let eq_int = (=)
+  let less_int = (<)
+  let ( * ) = ( * )
+  let (+) = (+)
+  let (-) = (-)
+  let (%) = (mod)
+  let (/) = (/)
 
-let put_char : char -> unit = fun c ->
-  let open Printf in
-  let n = Char.code c in
-  let dontEscape = (32 <= n && n <= 126) || n = 8 || c = '\n' in
-  if dontEscape then printf "%c%!" c else printf "%s%!" (Char.escaped c)
+  let ord = Char.code
+  let chr = Char.chr
 
-let eq_int : int -> int -> bool = fun x1 x2 ->
-  x1 = x2
+  let eq_char = (=)
+  let get_char () =
+    let bytes = Bytes.create 1 in
+    let n = Unix.read Unix.stdin bytes 0 1 in
+    if n < 1 then (Printf.printf "[EOF]\n"; exit 0) else
+      Bytes.get bytes 0
 
-let less_int : int -> int -> bool = fun x1 x2 ->
-  x1 < x2
+  let put_char : char -> unit = fun c ->
+    let open Printf in
+    let n = Char.code c in
+    let dontEscape = (32 <= n && n <= 126) || n = 8 || c = '\n' in
+    if dontEscape then printf "%c%!" c else printf "%s%!" (Char.escaped c)
 
-let ( * ) : int -> int -> int = ( * )
-let (+) : int -> int -> int = (+)
-let (-) : int -> int -> int = (-)
-let (%) : int -> int -> int = (mod)
-let (/) : int -> int -> int = (/)
-let ord : char -> int = Char.code
-let chr : int -> char = Char.chr
+  let upto i j =
+    let rec loop acc i = if i > j then List.rev acc else loop (i::acc) (i+1) in
+    loop [] i
 
-let upto : int -> int -> int list =
-  fun i j ->
-  let rec loop acc i = if i > j then List.rev acc else loop (i::acc) (i+1) in
-  loop [] i
+  let explode s =  (* TODO: define in example.fun, using lower level primitives *)
+    List.map (fun i -> s.[i]) (upto 0 (String.length s - 1))
 
-(* TODO: move explode into example.fun, defined using lower level primitives *)
-let explode : string -> char list =
-  fun s ->
-  List.map (fun i -> s.[i]) (upto 0 (String.length s - 1))
+end
+include X
