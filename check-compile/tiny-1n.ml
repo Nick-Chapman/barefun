30c135ffe978a625f9ad30496e68899405f58044
Nick Chapman <nic@amadido.co.uk>
2025-03-26 10:33:01 +0000

    normalize: WIP: walk exp tree; uniquify each binding

--- check-compile/tiny-1n.ml
+++ check-compile/tiny-1n.ml
@@ -1,21 +1,21 @@
 (*stage1*)
-let + = (fun x -> (fun y -> PRIM_AddInt(x,y))) in
-let - = (fun x -> (fun y -> PRIM_SubInt(x,y))) in
-let ( * ) = (fun x -> (fun y -> PRIM_MulInt(x,y))) in
-let / = (fun x -> (fun y -> PRIM_DivInt(x,y))) in
-let % = (fun x -> (fun y -> PRIM_ModInt(x,y))) in
-let < = (fun x -> (fun y -> PRIM_LessInt(x,y))) in
-let = = (fun x -> (fun y -> PRIM_EqInt(x,y))) in
-let eq_char = (fun x -> (fun y -> PRIM_EqChar(x,y))) in
-let ord = (fun x -> PRIM_CharOrd(x)) in
-let chr = (fun x -> PRIM_CharChr(x)) in
-let explode = (fun x -> PRIM_Explode(x)) in
-let put_char = (fun x -> PRIM_PutChar(x)) in
-let get_char = (fun x -> PRIM_GetChar(x)) in
-let :: = (fun x -> (fun y -> Cons1(x, y))) in
-let main = let mainloop = fix (fun mainloop _ ->
-  let gotten = (get_char Unit0) in
-  let _ = (put_char gotten) in
-  (mainloop Unit0)) in
-mainloop in
-(main Unit0)
+let +_3 = (fun x_1 -> (fun y_2 -> PRIM_AddInt(x_1,y_2))) in
+let -_6 = (fun x_4 -> (fun y_5 -> PRIM_SubInt(x_4,y_5))) in
+let ( * )_9 = (fun x_7 -> (fun y_8 -> PRIM_MulInt(x_7,y_8))) in
+let /_12 = (fun x_10 -> (fun y_11 -> PRIM_DivInt(x_10,y_11))) in
+let %_15 = (fun x_13 -> (fun y_14 -> PRIM_ModInt(x_13,y_14))) in
+let <_18 = (fun x_16 -> (fun y_17 -> PRIM_LessInt(x_16,y_17))) in
+let =_21 = (fun x_19 -> (fun y_20 -> PRIM_EqInt(x_19,y_20))) in
+let eq_char_24 = (fun x_22 -> (fun y_23 -> PRIM_EqChar(x_22,y_23))) in
+let ord_26 = (fun x_25 -> PRIM_CharOrd(x_25)) in
+let chr_28 = (fun x_27 -> PRIM_CharChr(x_27)) in
+let explode_30 = (fun x_29 -> PRIM_Explode(x_29)) in
+let put_char_32 = (fun x_31 -> PRIM_PutChar(x_31)) in
+let get_char_34 = (fun x_33 -> PRIM_GetChar(x_33)) in
+let ::_37 = (fun x_35 -> (fun y_36 -> Cons1(x_35, y_36))) in
+let main_43 = let mainloop_42 = fix (fun mainloop_39 __38 ->
+  let gotten_40 = (get_char_34 Unit0) in
+  let __41 = (put_char_32 gotten_40) in
+  (mainloop_39 Unit0)) in
+mainloop_42 in
+(main_43 Unit0)
--- haskell/src/Normalize.hs
+++ haskell/src/Normalize.hs
@@ -1,27 +1,100 @@
 -- | Normalize using NbE...
 module Normalize (normalize) where
 
-{-import Builtin (Builtin,evalBuiltin)
-import Control.Monad (ap,liftM)
-import Data.List (intercalate)
-import Data.Map (Map)
-import Data.Set (Set,singleton,(\\),union)
-import Interaction (Interaction(..))
-import Lines (Lines,bracket,onHead,onTail,indented)
+--import Builtin (Builtin,evalBuiltin)
+--import Data.List (intercalate)
 import Par4 (Position(..))
-import Stage0 (evalLit,apply,Literal,Id(..),Name(GeneratedName))
-import Stage1 (Ctag(..),provenanceExp)
-import Text.Printf (printf)
-import Value (Value(..),deUnit)
-import qualified Data.Map as Map
-import qualified Data.Set as Set (toList,fromList,unions,empty)
--}
-import qualified Stage1 as SRC
 
-type Transformed = SRC.Exp
+import Control.Monad (ap,liftM)
+import Data.Map (Map)
+import Stage0 (Id(..))
+import Stage1 (Exp(..),Arm(..))
+import qualified Data.Map as Map
 
 ----------------------------------------------------------------------
 -- Normalize
 
-normalize :: SRC.Exp -> Transformed
-normalize e = e -- TODO: here
+normalize :: Exp -> Exp
+normalize e = runM (walk env0 e)
+
+type Env = Map Id Id
+
+env0 :: Env
+env0 = Map.empty
+
+look :: Env -> Id -> Id
+look env x = maybe err id $ Map.lookup x env
+  where err = error (show ("Normalize.walk/Var",x))
+
+walk :: Env -> Exp -> M Exp
+walk env = \case
+  Var pos x -> do
+    pure $ Var pos (look env x)
+  Lit p x -> do
+    undefined $ Lit p x
+  ConTag p tag es -> do
+    es <- mapM (walk env) es
+    pure $ ConTag p tag es
+  Prim p b es -> do
+    es <- mapM (walk env) es
+    pure $ Prim p b es
+  Lam p x body -> do
+    x' <- fresh x
+    let env' = Map.insert x x' env
+    body <- walk env' body
+    pure $ Lam p x' body
+  RecLam f x body -> do
+    x' <- fresh x
+    f' <- fresh f
+    let env' = Map.insert x x' (Map.insert f f' env)
+    body <- walk env' body
+    pure $ RecLam f' x' body
+  App e1 p e2 -> do
+    e1 <- walk env e1
+    e2 <- walk env e2
+    pure $ App e1 p e2
+  Let p x rhs body -> do
+    rhs <- walk env rhs
+    x' <- fresh x
+    let env' = Map.insert x x' env
+    body <- walk env' body
+    pure $ Let p x' rhs body
+  Case scrut arms -> do
+    scrut <- walk env scrut
+    arms <- mapM (walkArm env) arms
+    pure $ Case scrut arms
+
+walkArm :: Env -> Arm -> M Arm
+walkArm env (ArmTag c xs body) = undefined $ do
+  -- TODO bind
+  body <- walk env body
+  pure $ ArmTag c xs body
+
+fresh :: Id -> M Id
+fresh Id{name,optPos} = do
+  u <- Fresh
+  let _optPos = Just (Position 8 9)
+  let optUnique = Just u
+  let y = Id { optUnique
+             , optPos
+             , name
+             }
+  pure y
+
+instance Functor M where fmap = liftM
+instance Applicative M where pure = Ret; (<*>) = ap
+instance Monad M where (>>=) = Bind
+
+data M a where
+  Ret :: a -> M a
+  Bind :: M a -> (a -> M b) -> M b
+  Fresh :: M Int
+
+runM :: M a -> a
+runM m0 = loop 1 m0 $ \_ x -> x
+  where
+    loop :: Int -> M a -> (Int -> a -> b) -> b
+    loop u m k = case m of
+      Ret x -> k u x
+      Bind m f -> loop u m $ \u x -> loop u (f x) k
+      Fresh -> k (u+1) u
