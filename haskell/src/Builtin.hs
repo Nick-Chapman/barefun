043ea7d63482343fff72e7017b7b3e1a7b0b7273
Nick Chapman <nic@amadido.co.uk>
2025-03-31 20:15:58 +0100

    builtin: classify as Pure/Impure

--- haskell/src/Builtin.hs
+++ haskell/src/Builtin.hs
@@ -11,9 +11,34 @@ data Builtin
   | Explode
   deriving (Show)
 
+data Semantics
+  = Pure ([Value] -> Value)
+  | Impure ([Value] -> (Value -> Interaction) -> Interaction)
+
 evalBuiltin :: Builtin -> [Value] -> (Value -> Interaction) -> Interaction
-evalBuiltin b vs k = do
-  let
+evalBuiltin b args =
+  case defineBuiltin b of
+    Impure f -> f args
+    Pure f -> \k -> k (f args)
+
+defineBuiltin :: Builtin -> Semantics
+defineBuiltin b =
+  case b of
+    PutChar -> Impure $ \vs k -> IPut (deChar (oneArg vs)) (k unit)
+    GetChar -> Impure $ \vs k -> case deUnit (oneArg vs) of () -> IGet (\c -> k (VChar c))
+    AddInt -> Pure (\vs -> VNum (uncurry (+) (twoArgs deNum deNum vs)))
+    SubInt -> Pure (\vs -> VNum (uncurry (-) (twoArgs deNum deNum vs)))
+    MulInt -> Pure (\vs -> VNum (uncurry (*) (twoArgs deNum deNum vs)))
+    DivInt -> Pure (\vs -> VNum (uncurry div (twoArgs deNum deNum vs)))
+    ModInt -> Pure (\vs -> VNum (uncurry mod (twoArgs deNum deNum vs)))
+    LessInt -> Pure (\vs -> mkBool (uncurry (<) (twoArgs deNum deNum vs)))
+    EqInt -> Pure (\vs -> mkBool (uncurry (==) (twoArgs deNum deNum vs)))
+    EqChar -> Pure (\vs -> mkBool (uncurry (==) (twoArgs deChar deChar vs)))
+    CharOrd -> Pure (\vs -> VNum ((fromIntegral . Char.ord) (deChar (oneArg vs))))
+    CharChr -> Pure (\vs -> VChar ((Char.chr . fromIntegral) (deNum (oneArg vs))))
+    Explode -> Pure (\vs -> mkList [ VChar c | c <- deString (oneArg vs) ])
+
+  where
     unit = VCons tUnit []
     oneArg = \case [v] -> v; _ -> err
     twoArgs c1 c2 = \case [v1,v2] -> (c1 v1, c2 v2); _ -> err
@@ -21,19 +46,4 @@ evalBuiltin b vs k = do
     deChar = \case VChar c -> c; _ -> err
     deString = \case VString s -> s; _ -> err
     err :: a
-    err = error (show (b,vs))
-
-  case b of
-    PutChar -> IPut (deChar (oneArg vs)) (k unit)
-    GetChar -> case deUnit (oneArg vs) of () -> IGet (\c -> k (VChar c))
-    AddInt -> k (VNum (uncurry (+) (twoArgs deNum deNum vs)))
-    SubInt -> k (VNum (uncurry (-) (twoArgs deNum deNum vs)))
-    MulInt -> k (VNum (uncurry (*) (twoArgs deNum deNum vs)))
-    DivInt -> k (VNum (uncurry div (twoArgs deNum deNum vs)))
-    ModInt -> k (VNum (uncurry mod (twoArgs deNum deNum vs)))
-    LessInt -> k (mkBool (uncurry (<) (twoArgs deNum deNum vs)))
-    EqInt -> k (mkBool (uncurry (==) (twoArgs deNum deNum vs)))
-    EqChar -> k (mkBool (uncurry (==) (twoArgs deChar deChar vs)))
-    CharOrd -> k (VNum ((fromIntegral . Char.ord) (deChar (oneArg vs))))
-    CharChr -> k (VChar ((Char.chr . fromIntegral) (deNum (oneArg vs))))
-    Explode -> k (mkList [ VChar c | c <- deString (oneArg vs) ])
+    err = error (show b)
