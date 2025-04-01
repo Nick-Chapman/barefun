fff10b392cdc7b77e73b18e9c63aebb89e22fbf0
Nick Chapman <nic@amadido.co.uk>
2025-04-01 21:39:57 +0100

    dune: generate check-compile rules for exepcted compilation output for all examples X stages

---
+++ check-compile/generate/main.ml
@@ -0,0 +1,17 @@
+
+let generate_rules x m =
+  Printf.printf {|
+(rule (alias runtest) (action (diff ../%s-%s.ml %s-%s.gen)))
+(rule (deps ../../haskell/main.exe ../../examples/%s.fun) (action (with-stdout-to %s-%s.gen (run ../../haskell/main.exe ../../examples/%s.fun -compile -%s))))
+ |} x m x m  x x m  x m
+
+let generate_rules_set example =
+  ["0";"1u";"1";"2";"3"]
+  |> List.iter (generate_rules example)
+
+let () =
+  Sys.readdir "../examples"
+  |> Array.to_list
+  |> List.sort String.compare
+  |> List.filter_map (Filename.chop_suffix_opt ~suffix:".fun")
+  |> List.iter generate_rules_set
