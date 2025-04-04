138814c319862631d08350602303a0e67b81bcf7
Nick Chapman <nic@amadido.co.uk>
2025-04-04 12:35:18 +0100

    allow select versions per examples in check-compile

--- check-compile/generate/main.ml
+++ check-compile/generate/main.ml
@@ -1,13 +1,24 @@
 
-let generate_rules x m =
+
+let select_versions_for_example = function
+  | "tiny" -> [0;1;2;3;4;5]
+  | _ -> [0;1;2;3;4]
+
+let suffix_of_version v =
+  if v >= 0 && v <= 4 then "ml" else
+    if v == 5 then "asm" else
+     failwith "suffix_of_version"
+
+let generate_rules x v =
+  let suf = suffix_of_version v in
+  let m = Printf.sprintf "%d" v in
   Printf.printf {|
-(rule (alias runtest) (action (diff ../%s-%s.ml %s-%s.gen)))
+(rule (alias runtest) (action (diff ../%s-%s.%s %s-%s.gen)))
 (rule (deps ../../haskell/main.exe ../../examples/%s.fun) (action (with-stdout-to %s-%s.gen (run ../../haskell/main.exe ../../examples/%s.fun -compile -%s))))
- |} x m x m  x x m  x m
+ |} x m suf x m  x x m  x m
 
 let generate_rules_set example =
-  ["0";"1";"2";"3";"4"]
-  |> List.iter (generate_rules example)
+  List.iter (generate_rules example) (select_versions_for_example example)
 
 let () =
   Sys.readdir "../examples"
---
+++ check-compile/tiny-5.asm
@@ -0,0 +1,37 @@
+(*Stage5 (ASM)*)
+start=L2
+L1:
+  call bios_get_char
+  mov [201], ax
+  mov ax, 201
+  call bios_put_char
+  mov [202], ax
+  mov ax, 201
+  call bios_put_char
+  mov [203], ax
+  mov bp, 101
+  mov bp, [bp]
+  mov ax, 103
+  mov ax, [ax]
+  mov bx, [bp]
+  jmp [bx]
+
+L2:
+  mov sp, #0
+  push #0
+  mov [102], sp
+  push #0
+  mov [103], sp
+  push #L1
+  push sp
+  mov [101], sp
+  push #0
+  mov [104], sp
+  mov bp, 101
+  mov bp, [bp]
+  mov ax, 104
+  mov ax, [ax]
+  mov bx, [bp]
+  jmp [bx]
+
+
