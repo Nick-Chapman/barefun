06268c0d003e2bfb903107eacee7efe0793ea94f
Nick Chapman <nic@amadido.co.uk>
2025-04-24 13:25:21 +0100

    begin setup dune to run nasm/qemu (no auto gen yet)

--- x86/runtime.asm
+++ x86/runtime.asm
@@ -172,4 +172,23 @@ Bare_num_to_char:
     ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Generated code will be appended here.
+
+%include CODE
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Check Size
+
+;; Size allocated in layout.asm:
+%assign As kernel_size_in_sectors       ; in sectors
+%assign Ab (As * sector_size)           ; in bytes
+
+;; Size we actually require:
+%assign Rb ($ - $$)                     ; in bytes
+%assign Rs (1 + ((Rb-1)/sector_size))   ; in sectors
+
+;%error sectors allocated: As (bytes = Ab)
+;%error sectors required: Rs (bytes = Rb)
+
+%if Rb>Ab
+%error Kernel sectors allocated: As, required: Rs
+%endif
