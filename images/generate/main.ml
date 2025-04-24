c6d30fb0a4abcd771a2d1361b6b4e10b701d42bc
Nick Chapman <nic@amadido.co.uk>
2025-04-24 21:49:32 +0100

    generate nasm/qemu rules for all examples (explicitly limiting just those which dont work yet)

---
+++ images/generate/main.ml
@@ -0,0 +1,36 @@
+
+(* TODO: maybe also have aliases to run in ocaml and haskell emulators *)
+
+let generate_rules x =
+  Printf.printf {|
+(rule
+ (deps ../../x86/runtime.asm ../../compile-examples/gen/%s-5.gen)
+ (action
+  (with-stdout-to %s.img
+   (run nasm -Werror -o %s.img -dCODE='../../compile-examples/gen/%s-5.gen' ../../x86/runtime.asm))))
+(rule
+ (alias %s.qemu)
+ (deps (universe) %s.img)
+ (action
+ (progn
+  (run chmod +w %s.img)
+  (run qemu-system-i386 -hda %s.img))))
+|} x x x x x x x x
+
+let allow_example = function
+  (* TODO: allow all examples when they work in nasm *)
+  | "put_int" -> false
+  | "tree" -> false
+  | "fib" -> false
+  | "shell" -> false
+  | "sham" -> false
+  | "filesystem" -> false
+  | _ -> true
+
+let () =
+  Sys.readdir "../examples"
+  |> Array.to_list
+  |> List.sort String.compare
+  |> List.filter_map (Filename.chop_suffix_opt ~suffix:".fun")
+  |> List.filter allow_example
+  |> List.iter generate_rules
