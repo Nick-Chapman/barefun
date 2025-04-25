5384280e29ac6e4cf11141721774a916e69d629a
Nick Chapman <nic@amadido.co.uk>
2025-04-25 17:57:08 +0100

    move images --> test/images

--- test/images/generate/main.ml
+++ test/images/generate/main.ml
@@ -4,10 +4,10 @@
 let generate_rules x =
   Printf.printf {|
 (rule
- (deps ../../x86/runtime.asm ../../compile-examples/gen/%s-5.gen)
+ (deps ../../../x86/runtime.asm ../../compile-examples/gen/%s-5.gen)
  (action
   (with-stdout-to %s.img
-   (run nasm -Werror -o %s.img -dCODE='../../compile-examples/gen/%s-5.gen' ../../x86/runtime.asm))))
+   (run nasm -Werror -o %s.img -dCODE='../../compile-examples/gen/%s-5.gen' ../../../x86/runtime.asm))))
 (rule
  (alias %s.qemu)
  (deps (universe) %s.img)
@@ -23,7 +23,7 @@ let allow_example = function
   | _ -> true
 
 let () =
-  Sys.readdir "../test/examples" (* TODO: depend on compile-examples/gen ? *)
+  Sys.readdir "../examples"
   |> Array.to_list
   |> List.sort String.compare
   |> List.filter_map (Filename.chop_suffix_opt ~suffix:".fun")
